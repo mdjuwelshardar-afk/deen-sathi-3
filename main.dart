83142d1380e6ecf5e0cf95e16c54ad8eb87a6460
mdjuwelshardar-afk <mdjuwelshardar@gmail.com>
2026-07-29 02:50:13 +0600

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,30 +1,68 @@
 import 'package:flutter/material.dart';
 import 'theme/app_colors.dart';
-import 'screens/main_nav_screen.dart';
+import 'services/app_settings.dart';
+import 'screens/home_screen.dart';
 
 void main() {
   runApp(const DeenSathiApp());
 }
 
-class DeenSathiApp extends StatelessWidget {
+class DeenSathiApp extends StatefulWidget {
   const DeenSathiApp({super.key});
+
+  @override
+  State<DeenSathiApp> createState() => _DeenSathiAppState();
+}
+
+class _DeenSathiAppState extends State<DeenSathiApp> {
+  @override
+  void initState() {
+    super.initState();
+    AppSettings.instance.load();
+    AppSettings.instance.addListener(_onSettingsChanged);
+  }
+
+  @override
+  void dispose() {
+    AppSettings.instance.removeListener(_onSettingsChanged);
+    super.dispose();
+  }
+
+  void _onSettingsChanged() {
+    if (mounted) setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Deen Sathi',
       debugShowCheckedModeBanner: false,
+      themeMode: AppSettings.instance.themeMode,
       theme: ThemeData(
         useMaterial3: true,
+        brightness: Brightness.light,
         scaffoldBackgroundColor: AppColors.cream,
         colorScheme: ColorScheme.fromSeed(
           seedColor: AppColors.emeraldDeep,
           primary: AppColors.emeraldDeep,
           secondary: AppColors.gold,
+          brightness: Brightness.light,
         ),
         fontFamily: 'Roboto',
       ),
-      home: const MainNavScreen(),
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: AppColors.darkBg,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: AppColors.emeraldMid,
+          primary: AppColors.emeraldMid,
+          secondary: AppColors.gold,
+          brightness: Brightness.dark,
+        ),
+        fontFamily: 'Roboto',
+      ),
+      home: const HomeScreen(),
     );
   }
 }
