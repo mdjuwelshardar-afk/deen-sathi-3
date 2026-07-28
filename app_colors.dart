a3061c901ea826bf951e287fc9663c0de6d48abb
mdjuwelshardar-afk <mdjuwelshardar@gmail.com>
2026-07-29 02:38:37 +0600

    Add files via upload

---
+++ app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+/// এই রঙগুলো deen-sathi-ui-mockup.html এর ডিজাইন থেকে নেওয়া।
+/// এখানে পরিবর্তন করলে পুরো অ্যাপের রঙ বদলে যাবে।
+class AppColors {
+  static const emeraldDeep = Color(0xFF0F3D3A);
+  static const emeraldMid = Color(0xFF1B5C55);
+  static const gold = Color(0xFFC9A24B);
+  static const cream = Color(0xFFF6F1E4);
+  static const ink = Color(0xFF1D231F);
+  static const muted = Color(0xFF7C8A83);
+}
